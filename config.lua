fc011db3c170a95de98fb916ac391fffca5cb54e
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-01-19 23:15:51 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -51,3 +51,20 @@ Config.WorkShops = {
 }
 
 return Config
+
+
+SVConfig = {}
+
+SVConfig.EnableESXSocietyPayment = true
+
+SVConfig.Webhook = {
+    botname = "Menu Custom Véhicules",
+    colors = { red = 16711680, blue = 255, yellow = 16776960, orange = 16744192, green = 65280},
+    list = {
+        ["mechanic"] = "https://discord.com/api/webhooks/1180122391800709190/TKhg99cAO5ARKg_q5RSqr9tWxW00djtV0WOaUnmLEVX9X7Jdts6UgXxEyryd8aQOiu9a",
+        ["mechanic2"] = "https://discord.com/api/webhooks/1180122699150938122/_gYehWGjnRt7lI3jS3w6rXIw4WMt2jVU5KD8i3sswsv2DhY_tUY_nuf6UfTfICULj3da",
+        ["admin"] = "https://discord.com/api/webhooks/1180122876058288168/awycaru_9o__z-2bv9sGxi4Lcw6y75URycvfM042hSRvUnOgolYJ4uXaz177yd_eJsB-",
+    }
+}
+
+return SVConfig
